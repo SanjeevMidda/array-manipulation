5d1fe8764e03b8d59aa49d937eaee25a3f4734e8
Sanjeev Midda <s.midda1@gmail.com>
2026-01-11 19:44:40 +0000

    added useState for list elements, .map to render these in the UI, and added buttons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./index.css";
+import { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [listOfNumbers, setListOfNumnbers] = useState([1, 2, 3, 4, 5]);
+
+  return (
+    <div className="App">
+      <div className="mainContainer">
+        <div className="listContainer">
+          {listOfNumbers.map((item, i) => {
+            return <p key={i}>{item}</p>;
+          })}
+        </div>
+        <div className="buttonContainer">
+          <button>Double All</button>
+          <button>Remove Even</button>
+          <button>Add Random</button>
+          <button>Remove First</button>
+          <button>Reverse</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
